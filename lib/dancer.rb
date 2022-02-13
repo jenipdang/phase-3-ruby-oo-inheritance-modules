352b37cb1840cf28fc6baa51d2632c6d1425c506
Jeni Dang <jenipdang@yahoo.com>
2022-02-13 00:07:58 -0800

    commit tree c26ac566ec7767fefb59a7efd3efc69198a16232

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    include Dance #allow class to use all of the methods of the included module as instance methods
+    extend MetaDancing
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        self.name = name
+    end
+end
